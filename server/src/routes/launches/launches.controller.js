1030307ecc38b36ccb6ca1963c41f9e75ebcf668
Prabhjot Singh <prabhjotsingh394@gmail.com>
2024-04-07 16:49:42 +0530

    POST /launches: Validation For POST Requests

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -6,7 +6,24 @@ function httpGetAllLaunches(req, res) {
 
 function httpAddNewLaunch(req, res) {
   const launch = req.body;
+
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.destination ||
+    !launch.launchDate
+  ) {
+    return res.status(400).json({
+      error: "Missing required launch property",
+    });
+  }
+
   launch.launchDate = new Date(launch.launchDate);
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({
+      error: "Invalid launch data",
+    });
+  }
 
   addNewLaunch(launch);
   return res.status(201).json(launch);
